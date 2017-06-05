dd70b6fefb1b769bd296d521705c07091d178db6
ohnoitsjmo <justin1998mo@gmail.com>
2017-06-05 01:49:40 -0700

    lol

---
+++ cpe202labs/lab7/hash_table_chaining.py
@@ -0,0 +1,72 @@
+# Justin Mo
+
+# class HashTable a wrapper class and is one of 
+# -- size or 
+# -- list of values 
+class HashTable:
+    def __init__(self):
+        self.size = 8 # an int
+        self.list_of_vals = [None] * 8 # Python built in array
+
+    def __repr__(self):
+        return "Size: {!r}, List: {!r}".format(self.size, self.list_of_vals)
+
+    def __eq__(self, other):
+        return type(other) == HashTable and self.size == other.size and self.list_of_vals == other.list_of_vals
+
+# class Value is one of
+# -- key or 
+# -- val
+class Value:
+    def __init__(self, key, val):
+        self.key = key # hash value
+        self.val = val # any value
+    
+    def __repr__(self):
+        return "(Key: {!r}, Val: {!r})".format(self.key, self.val)
+
+    def __eq__(self, other):
+        return type(other) == Value and self.key == other.key and self.val == other.val
+
+# None -> HashTable
+# Takes in no parameters and returns an empty hash table with of initial size 8.
+def empty_hash_table():
+    return HashTable()
+
+# HashTable key val -> HashTable
+# Takes in a hash table and a pair of a value and its python build in hash value. Returns a new hash table with the key and value inserted at its appropriate place.
+#def insert(table, key, val):
+     
+# HashTable key -> val 
+# Takes in a hash table and a key and returns the associated value at that key.
+def get(table, key):
+    index = key % (len(table.list_of_vals))
+    list_of_collisions = table.list_of_vals[index]
+    if list_of_collisions == None:
+        raise LookupError
+    for each_val in list_of_collisions:
+        if key == each_val.key:
+            return each_val.val
+    raise LookupError
+
+# HashTable key -> HashTable
+# Takes in a hash table and a key and removes the key-value pair from the hash table and returns the new hash table.
+def remove(table, key):
+    index = key % (len(table.list_of_vals))
+    list_of_collisions = table.list_of_vals[index]
+    counter = 0
+    if list_of_collisions == None:
+        raise LookupError
+    length = len(list_of_collisions)
+    for each_val in list_of_collisions:
+        if key == each_val.key:
+            if len(list_of_collisions) == 1:
+                table.list_of_vals[index] = None
+                return table
+            for i in range(counter, length - 1):
+                list_of_collisions[i] = list_of_collisions[i + 1]
+            table.list_of_vals[index] = list_of_collisions[0:length - 1]
+            return table
+        counter += 1    
+    raise LookupError 
+
---
+++ cpe202labs/lab7/hash_table_chaining_tests.py
@@ -0,0 +1,42 @@
+# Justin Mo
+import unittest
+from hash_table_chaining import *
+class TestHash(unittest.TestCase):
+    def test_empty_repr(self):
+        hash1 = empty_hash_table()
+        self.assertEqual(repr(hash1), "Size: 8, List: [None, None, None, None, None, None, None, None]")
+        val = Value(5, 5)
+        self.assertEqual(repr(val), "(Key: 5, Val: 5)")
+
+    def test_get(self):
+        hash1 = empty_hash_table()
+        hash1.list_of_vals = [[Value(0, 0)], [Value(1, 1)], None, None, None, None, None, [Value(7, 7)]]
+        self.assertEqual(get(hash1, 1), 1)
+        self.assertEqual(get(hash1, 7), 7)
+        with self.assertRaises(LookupError):
+            get(hash1, 2)
+        with self.assertRaises(LookupError):
+            get(hash1, 8)
+        
+    def test_remove(self):
+        hash1 = empty_hash_table()
+        hash1.list_of_vals = [[Value(8, 0)], [Value(1, 1)], None, None, None, None, None, [Value(7, 7)]]
+        hash2 = empty_hash_table()
+        hash2.list_of_vals = [None, [Value(1, 1)], None, None, None, None, None, [Value(7, 7)]]
+        self.assertEqual(remove(hash1, 8), hash2)
+        hash3 = empty_hash_table()
+        hash3.list_of_vals = [[Value(0, 0), Value(8, 0), Value(16, 0)], None, None, None, None, None, None, None]
+        hash4 = empty_hash_table()
+        hash4.list_of_vals = [[Value(0, 0), Value(16, 0)], None, None, None, None, None, None, None]
+        self.assertEqual(remove(hash3, 8), hash4)
+        hash5 = empty_hash_table()
+        hash5.list_of_vals = [[Value(0, 0), Value(8, 0), Value(16, 0)], None, None, None, None, None, None, None]
+        hash6 = empty_hash_table()
+        hash6.list_of_vals = [[Value(8, 0), Value(16, 0)], None, None, None, None, None, None, None] 
+        self.assertEqual(remove(hash5, 0), hash6)
+        with self.assertRaises(LookupError):
+            remove(hash6, 1)
+        with self.assertRaises(LookupError):
+            remove(hash6, 0)
+if __name__ == "__main__":
+    unittest.main()
